27c211105db1a94c68d8d3f70e027c3e5f74a5c0
Will Crawford <willcrawford@Parkers-MacBook-Air.local>
2020-08-06 15:10:10 -0400

    added leaderboard, total cash, and dealer title

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
 let maxScore = 21;
 //Put lose here after logic is figured out
 //Put win here
-let initalMoney = 1500;
+let totalCash = null;
 
 
 
-//You can save hgihest Cash reached with WebStorage API https://www.youtube.com/watch?v=NmXEJIBsN-4
+//You can save highest Cash reached with WebStorage API https://www.youtube.com/watch?v=NmXEJIBsN-4
 
 
 //*Cached Elements
 let leaderboarddBtn = document.getElementById("leaderboardButton");
-let leaderboardList = document.getElementById("leaderboardlist")
-
+let leaderboardList = document.getElementById("leaderboardList");
+let leaderboardNames = document.getElementById("leaderboardNames");
 
 
 
