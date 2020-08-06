0b20031633fb2a3e20761ca05d70739c1c9e0b8a
Will Crawford <willcrawford@Parkers-MacBook-Air.local>
2020-08-06 14:13:30 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//*Constant Variables
+let maxScore = 21;
+//Put lose here after logic is figured out
+//Put win here
+let initalMoney = 1500;
+
+
+
+//You can save hgihest Cash reached with WebStorage API https://www.youtube.com/watch?v=NmXEJIBsN-4
+
+
+//*Cached Elements
+let leaderboarddBtn = document.getElementById("leaderboardButton");
+let leaderboardList = document.getElementById("leaderboardlist")
+
+
+
+
+//Functions
